631d2a09683e41342ca858e75c31f83ed55b02d6
Booflame <Loekken.n@gmail.com>
2021-11-11 13:23:49 +0100

    opstart
    opstart

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector(".hamburger");
+const nav = document.querySelector("nav");
+
+// btn.addEventListener("click", () => btn.classList.toggle("is-active"))
+
+btn.addEventListener("click", function() {
+btn.classList.toggle("is-active")});
