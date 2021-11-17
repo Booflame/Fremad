35d8f01ab268b756980a06204bad89f9ff196b21
Booflame <Loekken.n@gmail.com>
2021-11-17 14:07:08 +0100

    fiksede hele hjemmesiden

--- js/script.js
+++
@@ -1,7 +0,0 @@
-const btn = document.querySelector(".hamburger");
-const nav = document.querySelector("nav");
-
-// btn.addEventListener("click", () => btn.classList.toggle("is-active"))
-
-btn.addEventListener("click", function() {
-btn.classList.toggle("is-active")});
